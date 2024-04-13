a210e545b20bf7b2909211fab780fbfa81b11c4f
Jakub Nezval <xnezval@fi.muni.cz>
2024-04-13 17:48:20 +0200

    feat(HanoiTowers): renaming 'Slot' to 'TowerIndex'

--- HanoiTowers/HanoiTowers.py
+++ HanoiTowers/HanoiTowers.py
@@ -2,53 +2,47 @@ import typing
 
 Tower = typing.List[int]
 Board = typing.Tuple[Tower, Tower, Tower]
-Slot = typing.Literal[0, 1, 2]
+TowerIndex = typing.Literal[0, 1, 2]
 
 
-def get_free_slot(
-        src_slot: Slot,
-        dst_slot: Slot
-) -> Slot:
-    return typing.cast(Slot, 3 - (src_slot +  dst_slot))
+def get_free_index(
+        src_index: TowerIndex,
+        dst_index: TowerIndex
+) -> TowerIndex:
+    return typing.cast(TowerIndex, 3 - (src_index + dst_index))
 
 
 def move_disc(
         board: Board,
-        src_slot: Slot,
-        dst_slot: Slot
+        src_index: TowerIndex,
+        dst_index: TowerIndex
 ) -> None:
-    board[dst_slot].append(board[src_slot].pop())
-
-    a, b, c = board
-    print("a: ", a)
-    print("b: ", b)
-    print("c: ", c)
-    print()
+    board[dst_index].append(board[src_index].pop())
 
 
 def move_disc_range(
         board: Board,
-        src_slot: Slot,
-        dst_slot: Slot,
+        src_index: TowerIndex,
+        dst_index: TowerIndex,
         range: int
 ) -> None:
     if range == 1:
-        move_disc(board, src_slot, dst_slot)
+        move_disc(board, src_index, dst_index)
         return
-    
-    free_slot = get_free_slot(src_slot, dst_slot)
 
-    move_disc_range(board, src_slot, free_slot, range - 1)
-    move_disc(board, src_slot, dst_slot)
-    move_disc_range(board, free_slot, dst_slot, range - 1)
+    free_index = get_free_index(src_index, dst_index)
+
+    move_disc_range(board, src_index, free_index, range - 1)
+    move_disc(board, src_index, dst_index)
+    move_disc_range(board, free_index, dst_index, range - 1)
 
 
 def move_tower(
         board: Board,
-        src_slot: Slot,
-        dst_slot: Slot
+        src_index: TowerIndex,
+        dst_index: TowerIndex
 ) -> None:
-    move_disc_range(board, src_slot, dst_slot, len(board[src_slot]))
+    move_disc_range(board, src_index, dst_index, len(board[src_index]))
 
 
 def main() -> int:
