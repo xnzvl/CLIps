e72883b600caa9f5b8572e8fddcc3df4def968a2
xnzvl <jakubnezval.jn@gmail.com>
2025-08-07 21:39:29 +0200

    feat(minesweeper/steps): compability with question marks

--- minesweeper/src/solving/strategy/steps.py
+++ minesweeper/src/solving/strategy/steps.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from random import choice
 from typing import Dict, List, Set, Tuple
 
@@ -6,10 +7,10 @@ from src.game.grids.grid import Grid
 from src.game.tiles.tile import Tile
 
 
-def random_move(board: Grid, covered_only: bool = True) -> List[Move]:
+def random_move(grid: Grid, covered_only: bool = True) -> List[Move]:
     random_moves: List[List[Move]] = list()
 
-    for point, tile in board:
+    for point, tile in grid:
         symbol = tile.get_symbol()
 
         if symbol == 'COVERED':
@@ -20,18 +21,33 @@ def random_move(board: Grid, covered_only: bool = True) -> List[Move]:
     return choice(random_moves) if len(random_moves) > 0 else []
 
 
-# TODO: code polish
-def certain_flags(board: Grid) -> List[Move]:
+def _flag_neighbours(covered_neighbours: List[Tuple[Point, Tile]], flag_moves: List[Move], already_flagged: Set[Point]) -> None:
+    for p, t in covered_neighbours:
+        covered_symbol = t.get_symbol()
+
+        if p in already_flagged or covered_symbol == 'FLAG':
+            continue
+
+        if covered_symbol == 'COVERED':
+            flag_moves.append(Move('secondary', p))
+        else:  # => covered_symbol == 'QUESTION_MARK'
+            flag_moves.extend([Move('secondary', p)] * 2)
+
+        already_flagged.add(p)
+        t.set_sign('FLAG')
+
+
+def certain_flags(grid: Grid) -> List[Move]:
     flag_moves: List[Move] = list()
     already_flagged: Set[Point] = set()
 
-    for point, tile in board:
+    for point, tile in grid:
         if tile.get_symbol() != 'NUMBER':
             continue
 
         covered_neighbours = [
             (p, t)
-            for p, t in board.get_neighbours_of_tile_at(point.x, point.y)
+            for p, t in grid.get_neighbours_of_tile_at(point.x, point.y)
             if t.is_covered()
         ]
         count = tile.get_count()
@@ -40,34 +56,22 @@ def certain_flags(board: Grid) -> List[Move]:
         if len(covered_neighbours) != count:
             continue
 
-        for p, t in covered_neighbours:
-            covered_symbol = t.get_symbol()
-
-            if p in already_flagged or covered_symbol == 'FLAG':
-                continue
-
-            if covered_symbol == 'COVERED':
-                flag_moves.append(Move('secondary', p))
-            else:  # => covered_symbol == 'QUESTION_MARK'
-                flag_moves.extend([Move('secondary', p)] * 2)
-
-            already_flagged.add(p)
-            t.set_sign('FLAG')
+        _flag_neighbours(covered_neighbours, flag_moves, already_flagged)
 
     return flag_moves
 
 
-def certain_safe_moves(board: Grid) -> List[Move]:
+def certain_safe_moves(grid: Grid) -> List[Move]:
     safe_moves: List[Move] = list()
 
-    for point, tile in board:
+    for point, tile in grid:
         if tile.get_symbol() != 'NUMBER':
             continue
 
         covered_neighbours: List[Tuple[Point, Tile]] = list()
         neighbour_flags = 0
 
-        for p, t in board.get_neighbours_of_tile_at(point.x, point.y):
+        for p, t in grid.get_neighbours_of_tile_at(point.x, point.y):
             symbol = t.get_symbol()
             if symbol == 'COVERED' or symbol == 'QUESTION_MARK':
                 covered_neighbours.append((p, t))
@@ -81,20 +85,20 @@ def certain_safe_moves(board: Grid) -> List[Move]:
     return safe_moves
 
 
-# TODO: make compatible with question-mark tiles
-# TODO: code polish
-def least_danger_moves(board: Grid) -> List[Move]:
+def _flag_reducer(accumulator: int, t: Tuple[Point, Tile]) -> int:
+    _, tile = t
+    return (accumulator + 1) if tile.get_symbol() == 'FLAG' else accumulator
+
+
+def _assess_danger_levels(grid: Grid) -> Dict[Point, Tuple[int, int]]:
     danger_levels: Dict[Point, Tuple[int, int]] = dict()
-    for point, tile in board:
+
+    for point, tile in grid:
         if tile.get_symbol() != 'NUMBER':
             continue
 
-        neighbours = board.get_neighbours_of_tile_at(point.x, point.y)
-        flags = 0
-
-        for neighbour_point, neighbour_tile in neighbours:
-            if neighbour_tile.get_symbol() == 'FLAG':
-                flags += 1
+        neighbours = grid.get_neighbours_of_tile_at(point.x, point.y)
+        flags = reduce(_flag_reducer, neighbours, 0)
 
         count = tile.get_count()
         assert count is not None
@@ -102,22 +106,29 @@ def least_danger_moves(board: Grid) -> List[Move]:
             raise RuntimeError('invalid state - too many flags')
 
         danger_increase = 1 if flags < count else 0
-        for neighbour_point, neighbour_tile in neighbours:
-            if neighbour_tile.get_symbol() == 'COVERED':
-                danger_level, max_danger = danger_levels.get(neighbour_point, (0, 0))
-                danger_levels[neighbour_point] = (danger_level + danger_increase, max_danger + 1)
+        for n_point, n_tile in neighbours:
+            symbol = n_tile.get_symbol()
+            if symbol == 'COVERED' or symbol == 'QUESTION_MARK':
+                danger_lvl, max_danger = danger_levels.get(n_point, (0, 0))
+                danger_levels[n_point] = (danger_lvl + danger_increase, max_danger + 1)
+
+    return danger_levels
+
+
+def least_danger_moves(grid: Grid) -> List[Move]:
+    danger_levels = _assess_danger_levels(grid)
 
     least_danger_moves_list: List[Move] = list()
     least_danger_level = 1
-    for point, levels in danger_levels.items():
-        current_level, max_level = levels
-        danger = current_level / max_level
+    for point, danger_stats in danger_levels.items():
+        current_danger_level, max_level = danger_stats
+        danger_level = current_danger_level / max_level
 
-        if danger < least_danger_level:
+        if danger_level < least_danger_level:
             least_danger_moves_list.clear()
-            least_danger_level = danger
+            least_danger_level = danger_level
 
-        if danger == least_danger_level:
+        if danger_level == least_danger_level:
             least_danger_moves_list.append(Move('primary', point))
 
     return least_danger_moves_list
