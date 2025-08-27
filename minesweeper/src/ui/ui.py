46eb75de4118f8eb332459dde1e74e17e201b529
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-08-27 02:13:49 +0200

    feat(minesweeper/ui): UI

---
+++ minesweeper/src/ui/ui.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+from src.game.grids.grid import Grid
+from src.game.literals import GameState, Result
+from src.ui.input import Input
+
+
+class UI(ABC):
+    @abstractmethod
+    def render_game_state(self, game_state: GameState) -> None:
+        ...
+
+    @abstractmethod
+    def render_grid(self, grid: Grid) -> None:
+        ...
+
+    @abstractmethod
+    def render_result(self, result: Result) -> None:
+        ...
+
+    @abstractmethod
+    def get_player_input(self) -> Input:
+        ...
