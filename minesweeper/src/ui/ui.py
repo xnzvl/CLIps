dfd3cf5821f13c75bba03a591fc203757ad5047d
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-08 23:21:04 +0200

    feat(minesweeper/ui): auto re-render time

--- minesweeper/src/ui/ui.py
+++ minesweeper/src/ui/ui.py
@@ -1,14 +1,29 @@
 from abc import ABC, abstractmethod
+from typing import Callable
 
 from src.game.grids.grid import Grid
 from src.game.sweeper import GameState, Result
 from src.ui.input import Input
+from src.ui.repeater import Repeater
 
 
 class UI(ABC):
+    def __init__(self) -> None:
+        self._repeater: Repeater | None = None
+
+    def start_rendering_time(self, time_getter: Callable[[], int]) -> None:
+        if self._repeater is None:
+            self._repeater = Repeater(1, lambda: self.render_time(time_getter()))
+            self._repeater.start()
+
+    def stop_rendering_time(self) -> None:
+        if self._repeater is not None:
+            self._repeater.stop()
+            self._repeater = None
+
     @abstractmethod
     def render_remaining_mines(self, remaining_mines: int) -> None:
-        pass
+        ...
 
     @abstractmethod
     def render_game_state(self, game_state: GameState) -> None:
