d9b79a0c58ae76976b6aa5aaadc9fd042f82d53f
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-16 09:44:26 +0200

    feat(minesweeper/utils): better config parsers

--- minesweeper/src/utils/configuration_parser.py
+++ minesweeper/src/utils/configuration_parser.py
@@ -4,37 +4,56 @@ from typing import List
 from src.common import WebPageSweeperConfiguration, Dimensions, Point, SweeperConfiguration
 
 
-def _add_core_arguments(parser: ArgumentParser) -> None:
-    parser.add_argument('width', metavar='WIDTH', type=int, help='width')
-    parser.add_argument('height', metavar='HEIGHT', type=int, help='height')
-    parser.add_argument('mines', metavar='MINES', type=int, help='number of mines')
+def _init_core_parser() -> ArgumentParser:
+    parser = ArgumentParser(add_help=False)
+
+    parser.add_argument('width',  metavar='WIDTH',  type=int, help='minefield width measured in tiles')
+    parser.add_argument('height', metavar='HEIGHT', type=int, help='minefield height measured in tiles')
+    parser.add_argument('mines',  metavar='MINES',  type=int, help='number of mines')
+
+    parser.add_argument(
+        '-qm', '--question_marks',
+        metavar='QUESTION_MARKS',
+        type=bool,
+        help='flag to signal whether question marks are enabled or not',
+        default=False
+    )
+
+    return parser
+
+
+SWEEPER_CONFIGURATION_CORE_PARSER = _init_core_parser()
 
 
 def parse_sweeper_configuration(argv: List[str]) -> SweeperConfiguration:
-    parser = ArgumentParser(prog=argv[0])
+    parser = ArgumentParser(
+        prog=argv[0],  # TODO: adjust
+        parents=[SWEEPER_CONFIGURATION_CORE_PARSER]
+    )
 
-    _add_core_arguments(parser)
     ns = parser.parse_args(argv[1:])
 
     return SweeperConfiguration(
         dimensions=Dimensions(ns.width, ns.height),
         mines=ns.mines,
-        question_marks=False  # TODO: actually parse
+        question_marks=ns.question_marks
     )
 
 
 def parse_web_page_sweeper_configuration(argv: List[str]) -> WebPageSweeperConfiguration:
-    parser = ArgumentParser(prog=argv[0])
+    parser = ArgumentParser(
+        prog=argv[0],  # TODO: adjust
+        parents=[SWEEPER_CONFIGURATION_CORE_PARSER]
+    )
 
     parser.add_argument('x_offset', metavar='X_OFFSET', type=int, help='X offset')
     parser.add_argument('y_offset', metavar='Y_OFFSET', type=int, help='Y offset')
 
-    _add_core_arguments(parser)
     ns = parser.parse_args(argv[1:])
 
     return WebPageSweeperConfiguration(
         dimensions=Dimensions(ns.width, ns.height),
         mines=ns.mines,
-        question_marks=False,  # TODO: actually parse
+        question_marks=ns.question_marks,
         offsets=Point(ns.x_offset, ns.y_offset)
     )
