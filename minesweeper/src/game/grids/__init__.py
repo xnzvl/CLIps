dbb7134dc688ad771b5c4c67b599d82479af7ef6
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-20 00:53:22 +0200

    fix(minesweeper/clip): mypy

--- minesweeper/src/game/grids/__init__.py
+++ minesweeper/src/game/grids/__init__.py
@@ -2,6 +2,7 @@ from .grid import Grid, GridIterator
 from .impl.generic_grid import GenericGrid
 from .impl.frozen_grid  import FrozenGrid
 
+
 __all__ = [
     'Grid', 'GridIterator',
     'GenericGrid',
