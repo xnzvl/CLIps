f584b5146c8ba25940412a3a39ba82f09e556e44
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-22 23:01:05 +0200

    todo(minesweeper/exceptions)

--- minesweeper/src/exceptions.py
+++ minesweeper/src/exceptions.py
@@ -1,6 +1,9 @@
-class InvalidGameStateError(Exception):
+# TODO: move to related packages
+
+class InvalidStateError(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
+
 
 class FrozenInstanceError(Exception):
     def __init__(self, message: str) -> None:
