9314b98c15e3fcc6c1aa932416bea3da8e2dc846
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-25 22:46:33 +0200

    feat(minesweeper/strategy): factory for implemented strategies

--- minesweeper/src/solving/strategy/strategy_factory.py
+++ minesweeper/src/solving/strategy/strategy_factory.py
@@ -1,2 +1,31 @@
+from src.solving.strategy import Strategy
+from src.solving.strategy.steps import LeastDangerGuess, RandomStep, CertainStep
+
+
 class StrategyFactory:
-    pass
+    @staticmethod
+    def get_random_strategy() -> Strategy:
+        return Strategy(
+            [
+                RandomStep()
+            ]
+        )
+
+    @staticmethod
+    def get_certain_strategy() -> Strategy:
+        return Strategy(
+            [
+                CertainStep(),
+                RandomStep()
+            ]
+        )
+
+    @staticmethod
+    def get_least_danger_strategy() -> Strategy:
+        return Strategy(
+            [
+                CertainStep(),
+                LeastDangerGuess(),
+                RandomStep()
+            ]
+        )
