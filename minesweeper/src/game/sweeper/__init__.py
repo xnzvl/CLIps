cfd41a57b977628b2722ad4f3dc10be888236fc7
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-26 23:20:32 +0200

    fix(minesweeper/sweeper): adaptation

--- minesweeper/src/game/sweeper/__init__.py
+++ minesweeper/src/game/sweeper/__init__.py
@@ -1,12 +1,14 @@
-from .sweeper import Sweeper
-from .game_state import GameState, Result
+from .game_state    import GameState, Result
+from .sweeper       import Sweeper
+from .sweeper_error import SweeperError
 from .impl.minefield       import Minefield
 from .impl.webpage_sweeper import WebPageSweeper
 
 
 __all__ = [
-    'Sweeper',
     'GameState', 'Result',
+    'Sweeper',
+    'SweeperError',
 
     'Minefield',
     'WebPageSweeper'
