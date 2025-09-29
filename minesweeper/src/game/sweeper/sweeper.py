8e1d79958ac9612627bb79e84ca2b9b90b319c88
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-29 21:50:23 +0200

    feat(minesweeper/sweeper): check if mine count makes sense

--- minesweeper/src/game/sweeper/sweeper.py
+++ minesweeper/src/game/sweeper/sweeper.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 from src.common import Move, SweeperConfiguration, Dimensioned
 from src.game.grids import Grid
-from src.game.sweeper import GameState
+from src.game.sweeper import GameState, SweeperError
 from src.game.tiles import Tile
 
 
@@ -12,7 +12,14 @@ class Sweeper[C: SweeperConfiguration](Dimensioned, ABC):
 
         self._configuration = configuration
 
-        # TODO: check mine count - if it makes sense or not
+        dimensions = self._configuration.dimensions
+        total_tiles = dimensions.width * dimensions.height
+
+        if total_tiles <= configuration.mines:
+            raise SweeperError(
+                'too many mines - ' +
+                f'required mines: {configuration.mines}, total tiles: {total_tiles}'
+            )
 
     def _check_grid_size[T: Tile](self, grid: Grid[T]) -> None:
         if grid.get_width() != self._configuration.dimensions.width:
