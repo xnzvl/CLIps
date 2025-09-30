58d79716a16d589210cdaf40183c9c76e3171f6b
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-09-30 17:20:21 +0200

    feat(minesweeper/BotFactory): Minefield bot

--- minesweeper/src/solving/bot/bot_factory.py
+++ minesweeper/src/solving/bot/bot_factory.py
@@ -1,7 +1,7 @@
-from src.common import WebPageSweeperConfiguration
-from src.game.sweeper.impl.webpage_sweeper import WebPageSweeper
-from src.solving.bot.bot import Bot
-from src.solving.strategy.strategy import Strategy
+from src.common import SweeperConfiguration, WebPageSweeperConfiguration
+from src.game.sweeper import Minefield, WebPageSweeper
+from src.solving.bot import Bot
+from src.solving.strategy import Strategy
 
 
 class BotFactory:
@@ -9,6 +9,14 @@ class BotFactory:
     def get_webpage_bot(configuration: WebPageSweeperConfiguration, strategy: Strategy, username: str) -> Bot:
         return Bot(
             WebPageSweeper(configuration),
+            strategy,
+            username
+        )
+
+    @staticmethod
+    def get_minefield_bot(configuration: SweeperConfiguration, strategy: Strategy, username: str) -> Bot:
+        return Bot(
+            Minefield(configuration),
             strategy,
             username
         )
