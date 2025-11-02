7445d0f83fe867c00d796738afaf9146835913f1
Jakub Nezval <jakubnezval.jn@gmail.com>
2025-11-02 20:56:33 +0100

    feat(minesweeper/bot): logging_batch_solve summary

--- minesweeper/src/solving/bot/logging_batch_solve.py
+++ minesweeper/src/solving/bot/logging_batch_solve.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from functools import partial
 from math import log10
 
@@ -11,17 +12,24 @@ from .bot import Bot
 TERMINAL = Terminal()
 
 
+@dataclass
+class Results:
+    failures:   int = 0
+    exceptions: int = 0
+
+
 def write_attempt(digits: int, attempt_number: int, result: str) -> None:
     print(f'    #{attempt_number+1:0{digits}} {result}')
 
 
-def exception_consumer(digits: int, i: int, exception: Exception) -> None:
+def exception_consumer(results: Results, digits: int, i: int, exception: Exception) -> None:
+    results.exceptions += 1
     write_attempt(digits, i, f'{TERMINAL.darkorchid('ERROR!')} {TERMINAL.darkorchid4(str(exception))}')
 
-
-def result_consumer(digits: int, i: int, result: Result) -> None:
-    formatter = TERMINAL.bright_green if result == GameState.VICTORY else TERMINAL.firebrick1
-
+def result_consumer(results: Results, digits: int, i: int, result: Result) -> None:
+    is_victory = result == GameState.VICTORY
+    results.failures += 0 if is_victory else 1
+    formatter = TERMINAL.bright_green if is_victory else TERMINAL.firebrick1
     write_attempt(digits, i, f'{formatter(result.value.capitalize())}')
 
 
@@ -35,10 +43,17 @@ def logging_batch_solve(bot: Bot, batch_size: int) -> None:
     # TODO: needs more getters from Bot
 
     digits = int(log10(batch_size)) + 1
-    bot.batch_solve(
+    results = Results()
+    victories = bot.batch_solve(
         batch_size,
-        partial(result_consumer, digits),
-        partial(exception_consumer, digits)
+        partial(result_consumer, results, digits),
+        partial(exception_consumer, results, digits)
     )
 
+    print()
+    print(f'  {TERMINAL.bright_white(f'Victories:  {victories          / batch_size * 100:.3f}%')}')
+    print(f'  {TERMINAL.bright_white(f'Failures:   {results.failures   / batch_size * 100:.3f}%')}')
+    print(f'  {TERMINAL.bright_white(f'Exceptions: {results.exceptions / batch_size * 100:.3f}%')}')
+    print()
+    print(f'  {TERMINAL.bright_white(f'Winrate: {victories / (batch_size - results.exceptions) * 100:.3f}%')}')
     print()
